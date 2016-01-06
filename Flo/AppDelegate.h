d91e7964262e358e9cbf44575ca62bd34b1855a7
John Zhao <joh.zhao@gmail.com>
2016-01-06 13:25:06 +0800

    Initial commit

---
+++ Flo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flo
+//
+//  Created by John Zhao on 1/6/16.
+//  Copyright © 2016 John Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flo
+//
+//  Created by John Zhao on 1/6/16.
+//  Copyright © 2016 John Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
